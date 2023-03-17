b6535a4cb4443adf11483c8aa2e67a94b40c5470
Brandon Ho <brandonho5@gmail.com>
2023-03-17 10:15:26 -0600

    New Photos

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -14,10 +14,16 @@ import Footer from "../Footer/Footer";
 import Navigation from "../Navigation/Navigation";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import firstPicture from "../assets/1.jpg";
-import secondPicture from "../assets/2.jpg";
-import cooper from "../assets/cooper.jpg";
-import cooperAnnie from "../assets/cooperAnnie.jpg";
+
+import Box from "../assets/Box.jpg";
+import IMG_4164 from "../assets/IMG_4164.jpg";
+import IMG_4166 from "../assets/IMG_4166.jpg";
+import IMG_4167 from "../assets/IMG_4167.jpg";
+import IMG_4168 from "../assets/IMG_4168.jpg";
+import IMG_4169 from "../assets/IMG_4169.jpg";
+import IMG_4170 from "../assets/IMG_4170.jpg";
+import IMG_4171 from "../assets/IMG_4171.jpg";
+import IMG_4172 from "../assets/IMG_4172.jpg";
 import "./Home.css";
 
 const Home = () => {
@@ -48,12 +54,38 @@ const Home = () => {
                   autoPlay={true}
                   width={400}
                   showThumbs={false}
+                  dynamicHeight={false}
                 >
                   <div>
-                    <img src={cooper} alt="cooper" />
+                    <img src={Box} alt="Box" />
                   </div>
                   <div>
-                    <img src={cooperAnnie} alt="cooperAnnie" />
+                    <img src={IMG_4164} alt="IMG_4164" />
+                  </div>
+                  <div>
+                    <img
+                      src={IMG_4166}
+                      alt="IMG_4166"
+                      style={{ maxHeight: "300px" }}
+                    />
+                  </div>
+                  <div>
+                    <img src={IMG_4167} alt="IMG_4167" />
+                  </div>
+                  <div>
+                    <img src={IMG_4168} alt="IMG_4168" />
+                  </div>
+                  <div>
+                    <img src={IMG_4169} alt="IMG_4169" />
+                  </div>
+                  <div>
+                    <img src={IMG_4170} alt="IMG_4170" />
+                  </div>
+                  <div>
+                    <img src={IMG_4171} alt="IMG_4171" />
+                  </div>
+                  <div>
+                    <img src={IMG_4172} alt="IMG_4172" />
                   </div>
                 </Carousel>
               </Stack.Item>
